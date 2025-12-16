4e9e2ce11111881a3ae9760063cb6e6ac5a26df2
0109-04 <0109-04@0109-04>
2025-12-16 11:56:33 +0300

    asd

---
+++ SQL/DBLECTION1216.sql
@@ -0,0 +1,28 @@
+--sp_addlogin
+
+--CREATE LOGIN ...
+
+--sp_addsrvrolemember
+
+--sp_adduser
+--CREATE USER ...
+
+GRANT SELECT, UPDATE(Price)
+ON Games
+TO login1
+
+DENY EXECUTE
+ON Procedure1
+TO guest
+
+DENY VIEW
+ANY DATABASE
+TO ÈלÿÂץמהא
+
+BACKUP DATABASE בה
+TO ...
+
+--PRO INDEXES SAM CHITAI BOT
+
+RESTORE DATABASE בה
+FROM ...
